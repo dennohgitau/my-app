77df1956f32eba339c941aa24e24eddf353915ba
Denis Gitau <denisgitaustudy@gmail.com>
2025-04-08 15:41:20 +0300

    Fix Jest hanging issue by properly closing server after tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
 const request = require('supertest');
-const app = require('../src/index');
+const { app, server } = require('../src/index');
 
 describe('GET /', () => {
   it('responds with Hello from my CI/CD pipeline with GitHub!', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toContain('Hello from my CI/CD pipeline with GitHub!');
+  });
+  
+  // Close the server after all tests
+  afterAll(done => {
+    server.close(done);
   });
 });
